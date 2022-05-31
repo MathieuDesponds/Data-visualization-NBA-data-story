57bbd5188aaee5f4003e5730df08da619462c6e4
Mathieu Desponds <mathieu.desponds@epfl.ch>
2022-05-31 15:31:46 +0200

    Changed HTMChanged HTMLL

--- js/viz1.js
+++ js/viz1.js
@@ -1,8 +1,7 @@
 import Team from './Team.js';
 import {selector} from './viz1_selectors.js'
-import {drawMap, drawCities, drawPaths} from './viz1_map.js'
+import {drawMap} from './viz1_map.js'
 
 drawMap()
 selector.showYearSelection()
-selector.showPeriodForStatisticsSelection()
 d3.csv(Team.TEAM_FILE,(data) => selector.showTeamsCheckbox(data.map(team => new Team(team))))
