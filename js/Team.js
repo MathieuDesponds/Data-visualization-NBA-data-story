52e17682da9e7ab8301f6b369a8df2e97c46aa0c
magantoine <62933498+magantoine@users.noreply.github.com>
2022-06-03 15:38:48 +0200

    remote teams summary

--- js/Team.js
+++ js/Team.js
@@ -1,5 +1,5 @@
 export default class Team{
-    static TEAM_FILE = '../data_web/teams_summary.csv';
+    static TEAM_FILE = 'https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-lebron-jenkins/master/data_web/teams_summary.csv';
     static TEAM_ABR_ON_LEFT = new Set(["POR","GSW","LAC","PHX","UTA","DEN","MIN","MIL","CHI","IND","DET","NYK","SAS","DAL","OKC","MEM","CLE"]);
     static MAX_NUMBER_OF_TEAMS = 2;
     constructor(team){
